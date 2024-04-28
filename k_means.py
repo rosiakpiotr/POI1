55549873992ef0e04382d22ca4f47e9a13eadc18
Piotr Rosiak <rosiakpiotr3@gmail.com>
2024-04-28 23:06:58 +0200

    K-Means reads from pre-merged all_points file now

--- k_means.py
+++ k_means.py
@@ -11,28 +11,36 @@ def point_cloud_read(filename: str):
             yield float(x), float(y), float(z)
 
 
-horizontal_plane_points = list(point_cloud_read('horizontal_plane.xyz'))
-vertical_plane_points = list(point_cloud_read('vertical_plane.xyz'))
-cylinder_points = list(point_cloud_read('vertical_cylinder.xyz'))
-
-all_points = np.array([*horizontal_plane_points, *vertical_plane_points, *cylinder_points])
+all_points = np.array(list(point_cloud_read('all_points.xyz')))
 
 classifier = KMeans(n_clusters=3)
 clustered = classifier.fit_predict(all_points)
 
-h = clustered == 0
-v = clustered == 1
-c = clustered == 2
+# Actually nothing tells which clustered part is what
+# setting variable names to 'horizontal' or 'vertical'
+# is not correct as there is no way to tell.
+horizontal = clustered == 0
+vertical = clustered == 1
+cylinder = clustered == 2
 
 
-def plot_clustered(data, results):
+def plot_clustered(data, results, title: str):
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
     ax.scatter(data[results, 0], data[results, 1], data[results, 2], s=0.5)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+    plt.title(title)
     plt.show(block=False)
 
 
-plot_clustered(all_points, h)
-plot_clustered(all_points, v)
-plot_clustered(all_points, c)
+def prepare_title(num, results, total_count):
+    return f'Klaster {num} ({100 * results.sum() / total_count}% punktów)'
+
+
+point_count = len(all_points)
+plot_clustered(all_points, horizontal, prepare_title(1, horizontal, point_count))
+plot_clustered(all_points, vertical, prepare_title(2, vertical, point_count))
+plot_clustered(all_points, cylinder, prepare_title(3, cylinder, point_count))
 plt.show()
